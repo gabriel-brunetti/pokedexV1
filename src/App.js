3043c01255ee383a77744c99c28da64efff83f9c
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2020-10-30 21:21:24 -0300

    projeto ao final da aula

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Pokemon from './components/Pokemon'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemons: [],
+      pokemonsDetails: [],
+      offset: 0,
+      loadNumber: 6,
+    };
+    this.handleMoreClick = this.handleMoreClick.bind(this);
+  }
+
+  getNextOffset() {
+    return this.state.offset+this.state.loadNumber
+  }
+
+  handleMoreClick(event) {
+    const newOffset = this.getNextOffset();
+    this.setState({offset: newOffset}, ()=> {
+      console.log("Offset: " + this.state.offset)
+      this.getMorePokemon()
+    })
+  }
+
+  componentDidMount() {
+    this.getMorePokemon();
+  }
+
+  getMorePokemon() {
+    let url ="https://pokeapi.co/api/v2/pokemon?offset=" + this.state.offset +"&limit=" + this.state.loadNumber;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          this.setState({ pokemons: data.results })
+          
+          this.state.pokemons.map((pokemon) => {
+            fetch(pokemon.url)
+              .then((response) => response.json())
+              .then((data) => {
+                if (data) {
+                  let temp = this.state.pokemonsDetails;
+                  temp.push(data);
+                  this.setState({ pokemonsDetails: temp });
+                }
+              })
+              .catch(console.log);
+          });
+        };
+      })
+      .catch(console.log);
+    }
+
+  render() {
+    const { pokemonsDetails } = this.state;
+
+    const renderedPokemonList = pokemonsDetails.map((pokemon, index) => {
+      return (<Pokemon pokemon={pokemon} />);
+    });
+
+    return (
+      <div className="container">
+        <div className="card-columns">{renderedPokemonList}</div>
+        <button 
+          type="button" 
+          className="btn btn-secondary btn-block" 
+          key="more-button" 
+          onClick={this.handleMoreClick}>
+            Carregar Mais
+          </button>
+      </div>
+    );
+  }
 }
 
 export default App;
